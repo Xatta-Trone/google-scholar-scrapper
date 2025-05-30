c0feaa6282fbd1d99cd7a7ddf1afd8d61e13dca1
xatta-trone <monzurul.ce.buet@gmail.com>
2025-05-30 10:06:02 -0500

    update date fix

--- 1_gs2.js
+++ 1_gs2.js
@@ -245,7 +245,11 @@ const scholarParser = {
 
       return data;
     });
-    articleData = { ...articleData, ...articleMetaData, date_added: (new Date()).toISOString.split('T')[0] };
+    articleData = {
+      ...articleData,
+      ...articleMetaData,
+      date_added: new Date().toISOString().split('T')[0],
+    };
     console.log(articleData);
 
     return articleData;
