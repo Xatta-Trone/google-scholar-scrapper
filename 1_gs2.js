8311225e6ec0790da3347d71a4e484b8209de0eb
Md Monzurul Islam <monzurul.ce.buet@gmail.com>
2026-08-24 16:23:17 -0600

    Update userIds array in 1_gs2.js
    Removed one user ID from the list of Google Scholar user IDs.

--- 1_gs2.js
+++ 1_gs2.js
@@ -271,7 +271,7 @@ const scholarParser = {
 };
 
 // List of Google Scholar user IDs
-const userIds = ["qK-YgxAAAAAJ", "edkjFpwAAAAJ"];
+const userIds = ["edkjFpwAAAAJ"];
 // const userIds = ["U9tD0ywAAAAJ"];
 
 // Run the parser for each user ID
